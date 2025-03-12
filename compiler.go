0303543a876396609b0c3b7c15a15e9c1bd63a91
Shivansh Sharma <shivansh.report@gmail.com>
2025-03-12 18:11:20 +0530

    Commit before making major change to the compiler

--- compiler.go
+++ compiler.go
@@ -1,11 +1,72 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Row struct {
+	ID       int
+	Username string
+	Email    string
+}
+
+type Table struct {
+	Rows []Row
+}
+
+func NewTable() *Table {
+	return &Table{
+		Rows: make([]Row, 0),
+	}
+}
+
+type Opcode int
+const (
+	OpInsert Opcode = iota
+	OpSelect
+)
+
+type Instruction struct {
+	Op   Opcode
+	Args []interface{}
+}
+
+type Program struct {
+	Instructions []Instruction
+}
+
 type Compiler struct{}
 
 func NewCompiler() *Compiler {
 	return &Compiler{}
 }
 
-// func (c *Compiler) Compile(input string) (*Program, error) {
+func (c *Compiler) Compile(input string) (*Program, error) {
+	tokens := strings.Fields(input)
+	if len(tokens) == 0 {
+		return nil, fmt.Errorf("Empty Input")
+	}
 
-// }
+	program := &Program{}
+
+	switch strings.ToLower(tokens[0]) {
+	case "insert":
+		id, _ := strconv.Atoi(tokens[1])
+		program.Instructions = append(program.Instructions, Instruction{
+			Op: OpInsert,
+			Args: []interface{}{id, tokens[2], tokens[3]},
+		})
+	case "select":
+		program.Instructions = append(program.Instructions, Instruction{
+			Op: OpSelect,
+		})
+
+	default:
+		return nil, fmt.Errorf("Unrecognized command: %s", tokens[0])
+	}
+
+	return program, nil
+}
+
--- main.go
+++ main.go
@@ -8,18 +8,32 @@ import (
 )
 
 func main() {
+	compiler := NewCompiler()
 	reader := bufio.NewReader(os.Stdin)
-	for {
+
+	// REPL 
+	for { 
 		fmt.Print("gokulite> ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
-		if input == ".exit" {
-			fmt.Println("Exiting Gokulite. Goodbye!")
-			break
+
+		// Handle the Meta command
+		if input[0] == '.' {
+			if input == ".exit" {
+				fmt.Println("Exiting Gokulite. Goodbye!");
+				break
+			} else {
+				fmt.Printf("Unrecognized Meta Command: %s\n", input);
+			}
+		} else {
+			// Handle the compiler command
+			program, err := compiler.Compile(input)
+			if err != nil {
+				fmt.Println("Compilation Error: ", err)
+				continue
+			}
+			fmt.Printf("Unrecognized command: %s\n", input)
+			fmt.Println("Compiled Program", program)
 		}
-		fmt.Printf("Unrecognized command: %s\n", input)
 	}
-
-	
-
 }
