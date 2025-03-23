db0367c9f066f2b8f2b3eba6902d79ec9be07ac7
Shivansh Sharma <shivansh.report@gmail.com>
2025-03-23 21:56:16 +0530

    Refactoring codebase

--- main.go
+++ main.go
@@ -7,20 +7,10 @@ import (
 	"strings"
 )
 
-func SaveData(path string, data []byte) error {
-	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
-
-	if err != nil {
-		return err
-	}
-	defer fp.Close()
-
-	_, err = fp.Write(data)
-	if err != nil {
-		return err
-	}
-
-	return fp.Sync()
+type Statement struct {
+	statementType    string
+	statementData    []any
+	statementSuccess bool
 }
 
 func main() {
@@ -53,7 +43,11 @@ func main() {
 	table.Rows = append(table.Rows, Row{ID: "3", Username: "John", Email: "John@Gmail.com"})
 	databaseFile.Write(table.SerializeAllRows())
 
-	fmt.Println(databaseFile)
+	fileScanner := bufio.NewScanner(databaseFile)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
 
 	// REPL
 	for {
